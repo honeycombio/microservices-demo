d01ab8dacb7f2c458e7c5154ac60f97b466c1594
Michael Sickles <michael.c.sickles@gmail.com>
2021-03-02 16:23:40 -0500

    nodejs

---
+++ src/paymentservice/tracing.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const grpc = require('grpc');
+const { NodeTracerProvider } = require("@opentelemetry/node");
+const { SimpleSpanProcessor } = require("@opentelemetry/tracing");
+const { CollectorTraceExporter } = require("@opentelemetry/exporter-collector-grpc");
+const { registerInstrumentations } = require('@opentelemetry/instrumentation');
+const provider = new NodeTracerProvider({
+    plugins: {
+        grpc: {
+            enabled: true,
+            // You may use a package name or absolute path to the file.
+            path: '@opentelemetry/plugin-grpc',
+            // gRPC plugin options
+        },
+        http: {
+            enabled: true,
+            // You may use a package name or absolute path to the file.
+            path: '@opentelemetry/plugin-http',
+            // http plugin options
+        }
+    }
+}
+);
+
+const metadata = new grpc.Metadata();
+metadata.set("x-honeycomb-team", process.env.HONEYCOMB_API_KEY);
+metadata.set("x-honeycomb-dataset", process.env.HONEYCOMB_DATASET);
+
+const collectorOptions = {
+    serviceName: process.env.SERVICE_NAME,
+    url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
+    credentials: grpc.credentials.createSsl(),
+    metadata
+};
+
+provider.addSpanProcessor(
+    new SimpleSpanProcessor(
+        new CollectorTraceExporter(collectorOptions)
+    )
+);
+
+provider.register();
+registerInstrumentations({
+    tracerProvider: provider,
+});
+console.log("tracing initialized");
