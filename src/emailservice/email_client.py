a499a211458107f48ae84fce01ce6c447ad899ad
Pierre Tessier <pierre@pierretessier.com>
2022-01-06 17:32:25 -0500

    optimize imports

--- src/emailservice/email_client.py
+++ src/emailservice/email_client.py
@@ -22,25 +22,25 @@ from random import randint
 from time import sleep
 
 from logger import getJSONLogger
+
 logger = getJSONLogger('emailservice-client')
 
 
 def send_confirmation_email(email, order):
-  channel = grpc.insecure_channel('0.0.0.0:8080')
-  # channel = grpc.intercept_channel(channel)
-  stub = demo_pb2_grpc.EmailServiceStub(channel)
+    channel = grpc.insecure_channel('0.0.0.0:8080')
+    # channel = grpc.intercept_channel(channel)
+    stub = demo_pb2_grpc.EmailServiceStub(channel)
 
-  sleep(randint(10,250)/1000)
+    try:
+        response = stub.SendOrderConfirmation(demo_pb2.SendOrderConfirmationRequest(
+            email=email,
+            order=order
+        ))
+        logger.info('Request sent.')
+    except grpc.RpcError as err:
+        logger.error(err.details())
+        logger.error('{}, {}'.format(err.code().name, err.code().value))
 
-  try:
-    response = stub.SendOrderConfirmation(demo_pb2.SendOrderConfirmationRequest(
-      email = email,
-      order = order
-    ))
-    logger.info('Request sent.')
-  except grpc.RpcError as err:
-    logger.error(err.details())
-    logger.error('{}, {}'.format(err.code().name, err.code().value))
 
 if __name__ == '__main__':
-  logger.info('Client for email service.')
+    logger.info('Client for email service.')
