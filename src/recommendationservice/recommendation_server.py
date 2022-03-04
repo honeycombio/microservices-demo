82b72f75be44d84d8f879085007cc3f947b35bf0
Michael Sickles <michael.c.sickles@gmail.com>
2022-03-04 13:28:54 -0500

    Fix recommendation

--- src/recommendationservice/recommendation_server.py
+++ src/recommendationservice/recommendation_server.py
@@ -28,10 +28,6 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from logger import getJSONLogger
 
-from opentelemetry.propagate import set_global_textmap
-from opentelemetry.propagators.b3 import B3Format
-
-set_global_textmap(B3Format())
 
 logger = getJSONLogger('recommendationservice-server')
 
