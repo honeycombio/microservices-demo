1271a7cadebffe3c96e41633a340c402f4065469
Pierre Tessier <pierre@pierretessier.com>
2022-05-06 13:51:11 -0400

    deploy marker link

--- src/frontend/cache.go
+++ src/frontend/cache.go
@@ -88,7 +88,7 @@ func (c *CacheTracker) createMarker() {
 	c.log.Debug("Creating Honeycomb marker...")
 
 	url := "https://api.honeycomb.io/1/markers/" + c.honeycombDataset
-	payload := []byte(`{"message":"Deploy C34E68A7", "url":"https://github.com/honeycombio/microservices-demo/commit/36bbbc36afebf145a992a7446554fb5371be149f", "type":"deploy"}`)
+	payload := []byte(`{"message":"Deploy 5645075", "url":"https://github.com/honeycombio/microservices-demo/commit/5645075", "type":"deploy"}`)
 	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(payload))
 	if err != nil {
 		c.log.Error(errors.Wrap(err, "could not create request to generate marker"))
