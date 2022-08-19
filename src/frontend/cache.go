51e2476dffcb2d357eda7f43918ee05cb175ae8f
Pierre Tessier <pierre@pierretessier.com>
2022-08-19 11:31:52 -0400

    remove honeycomb classic stuff

--- src/frontend/cache.go
+++ src/frontend/cache.go
@@ -13,30 +13,25 @@ import (
 )
 
 type CacheTracker struct {
-	currentSize            int
-	userThreshold          int
-	markerThreshold        int
-	honeycombAPIKey        string
-	honeycombAPIKeyClassic string
-	honeycombDataset       string
-	log                    logrus.FieldLogger
-	lock                   sync.Mutex
+	currentSize     int
+	userThreshold   int
+	markerThreshold int
+	honeycombAPIKey string
+	log             logrus.FieldLogger
+	lock            sync.Mutex
 }
 
-func NewCacheTracker(userThreshold, markerThreshold int, apiKey, apiKeyClassic, dataset string, log logrus.FieldLogger) *CacheTracker {
+func NewCacheTracker(userThreshold, markerThreshold int, apiKey string, log logrus.FieldLogger) *CacheTracker {
 	log.WithFields(logrus.Fields{
-		"userThreshold":    userThreshold,
-		"markerThreshold":  markerThreshold,
-		"honeycombDataset": dataset,
+		"userThreshold":   userThreshold,
+		"markerThreshold": markerThreshold,
 	}).Debug("Creating Cache Tracker")
 
 	return &CacheTracker{
-		userThreshold:          userThreshold,
-		markerThreshold:        markerThreshold,
-		honeycombAPIKey:        apiKey,
-		honeycombAPIKeyClassic: apiKeyClassic,
-		honeycombDataset:       dataset,
-		log:                    log,
+		userThreshold:   userThreshold,
+		markerThreshold: markerThreshold,
+		honeycombAPIKey: apiKey,
+		log:             log,
 	}
 }
 
@@ -116,38 +111,5 @@ func (c *CacheTracker) createMarkerHoneycomb() {
 		c.log.Error(string(bodyBytes))
 	} else {
 		c.log.Debug("Honeycomb marker created.")
-	}
-}
-
-func (c *CacheTracker) createMarkerHoneycombClassic() {
-
-	if c.honeycombAPIKeyClassic == "" || c.honeycombDataset == "" {
-		// Do nothing
-		return
-	}
-	c.log.Debug("Creating Honeycomb marker...")
-
-	url := "https://api.honeycomb.io/1/markers/" + c.honeycombDataset
-	payload := []byte(`{"message":"Deploy 5645075", "url":"https://github.com/honeycombio/microservices-demo/commit/5645075", "type":"deploy"}`)
-	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(payload))
-	if err != nil {
-		c.log.Error(errors.Wrap(err, "could not create request to generate marker"))
-		return
-	}
-	req.Header.Set("X-Honeycomb-Team", c.honeycombAPIKeyClassic)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-
-	if err != nil {
-		c.log.Error(errors.Wrap(err, "could not create Honeycomb Classic marker"))
-	} else if resp.StatusCode != 200 && resp.StatusCode != 201 {
-		c.log.WithFields(logrus.Fields{
-			"satusCode": resp.StatusCode,
-		}).Error("Invalid status code when creating Honeycomb Classic marker")
-		bodyBytes, _ := io.ReadAll(resp.Body)
-		c.log.Error(string(bodyBytes))
-	} else {
-		c.log.Debug("Honeycomb Classic marker created.")
 	}
 }
