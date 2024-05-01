379c0e37da92d0e41cbbef6412f96a24420080bc
sgsharma <sashasharma@honeycomb.io>
2024-04-30 19:04:30 -0700

    remove unused imports

--- src/recommendationservice/logger.py
+++ src/recommendationservice/logger.py
@@ -8,9 +8,6 @@ from opentelemetry.exporter.otlp.proto.grpc._log_exporter import \
     OTLPLogExporter
 from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
 from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
-from opentelemetry.sdk.resources import Resource
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from pythonjsonlogger import jsonlogger
 
 
