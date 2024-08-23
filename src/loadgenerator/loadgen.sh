955b5b47c743a7f6bd8ce6a2d9225ff97fc906b4
howardyoo <howardyoo@gmail.com>
2024-08-23 15:23:24 -0500

    fixes issue #80

--- src/loadgenerator/loadgen.sh
+++ src/loadgenerator/loadgen.sh
@@ -10,12 +10,16 @@ fi
 
 set -x
 
-# if one request to the frontend fails, then exit
-STATUSCODE=$(curl --silent --output /dev/stderr --write-out "%{http_code}" "${FRONTEND_ADDR}")
-if test $STATUSCODE -ne 200; then
-    echo "Error: Could not reach frontend - Status code: ${STATUSCODE}"
-    exit 1
-fi
+# Keep trying until the frontend is reachable
+while true; do
+    STATUSCODE=$(curl --silent --output /dev/stderr --write-out "%{http_code}" "${FRONTEND_ADDR}")
+    if test $STATUSCODE -eq 200; then
+        break
+    else
+        echo "Error: Could not reach frontend - Status code: ${STATUSCODE}"
+        sleep 1
+    fi
+done
 
 # else, run loadgen
 locust --host="${FRONTEND_ADDR}" --headless -u "${USERS:-10}" 2>&1
